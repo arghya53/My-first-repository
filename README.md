2f8d163bc41a18b864a9199bd32b7593b9237b8d
arghya53 <90865141+arghya53@users.noreply.github.com>
2021-09-17 13:01:57 +0600

    Update and rename README.md to README.py
    I wrote the code to open a tsv file.

--- README.md
+++
@@ -1,2 +0,0 @@
-# My-first-repository
-How to open a tsv file in jupyter notebook.
---
+++ README.py
@@ -0,0 +1,6 @@
+# My-first-repository
+# How to open a tsv file in jupyter notebook.
+import pandas as pd
+df= pd.csv_read("file directory/file_name.tsv", delimiter='\t' ) #delimiter concatanates the columns and represent it like a csv file
+# for example df = pd.read_csv("D:/DataAnalysis/top_500_albums.tsv", delimiter='\t')
+df # it prints the dataFrame
