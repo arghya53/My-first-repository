9c2de46ade7a6e8371c56339375d90a2f2c366de
arghya53 <90865141+arghya53@users.noreply.github.com>
2021-09-17 13:26:51 +0600

    Update README.py

--- README.py
+++ README.py
@@ -2,5 +2,6 @@
 # How to open a tsv file in jupyter notebook.
 import pandas as pd
 df= pd.csv_read("file directory/file_name.tsv", delimiter='\t' ) #delimiter concatanates the columns and represent it like a csv file
+# the df is storing the tsv file in a dataframe
 # for example df = pd.read_csv("D:/DataAnalysis/top_500_albums.tsv", delimiter='\t')
 df # it prints the dataFrame
